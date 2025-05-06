4f84c686c077fafb023f34993d865c6ea6030ca4
lotinmaria <mariaholotin2007@gmail.com>
2025-05-06 15:51:47 -0300

    feat: atualização

--- garagem_app/models/veiculo.py
+++ garagem_app/models/veiculo.py
@@ -4,7 +4,7 @@ from garagem_app.models import Cor, Modelo, Acessorio
 class Veiculo(models.Model):
     ano = models.IntegerField(default=0, blank=True, null=True)
     preco = models.DecimalField(default=0, max_digits=10, decimal_places=2, blank=True, null=True)
-    acessorio = models.ManyToManyField(Acessorio,  null=True, blank=True)
+    acessorio = models.ManyToManyField(Acessorio, blank=True)
     cor = models.ForeignKey(Cor, on_delete=models.CASCADE, null=True, blank=True)
     modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE, null=True, blank=True)
     
